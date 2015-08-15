4efbd5fe166161ed09a6e1a3128d8766e550f57b
Artur Tsurkan <arturtsurkan@gmail.com>
2015-08-15 05:24:38 -0400

    Setting up the organization for the other components.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+router.get('/list', function(req, res, next) {
+  res.render('list');
+});
+
+router.get('/create', function(req, res, next) {
+  res.render('create');
+});
+
 
 module.exports = router;
