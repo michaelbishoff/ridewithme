390a91067ab9c5a894957182b6b1ff0b1f1f528a
Michael Bishoff <bishoff1@umbc.edu>
2015-08-16 08:05:09 -0700

    MVP

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var ffmpeg = require('fluent-ffmpeg');
 var mkdirp = require('mkdirp');
+//var Parse = require('parse').Parse;
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
