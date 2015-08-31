98270d577147d23cdac6db187d2a228315a9afa7
Michael Bishoff <bishoff1@umbc.edu>
2015-08-31 02:13:10 -0400

    Removed twilio because money

--- routes/index.js
+++ routes/index.js
@@ -30,36 +30,40 @@ router.get('/create', function(req, res, next) {
   res.render('create');
 });
 
+router.get('/shirley', function(req, res, next) {
+  res.render('shirley');
+});
+
 router.get('/ride/:objectId', function(req, res, next) {
    res.render('ride', {rideId : req.params.objectId});
 });
 
 router.get('/twilio', function(req, res, next) {
-    var client = new twilio.RestClient('ACcdc8ccd4745762159ddaaa5cf8993524', '39955f2b0dea465315357ffa8b3832b3');
-    console.log(req.query['message']);
-    
-    client.sms.messages.create({
-        to:'+13017871566',
-        from:'+16198666670',
-        body:req.query['message']
-    }, function(error, message) {
-        // The HTTP request to Twilio will run asynchronously. This callback
-        // function will be called when a response is received from Twilio
-        // The "error" variable will contain error information, if any.
-        // If the request was successful, this value will be "falsy"
-        if (!error) {
-            // The second argument to the callback will contain the information
-            // sent back by Twilio for the request. In this case, it is the
-            // information about the text messsage you just sent:
-            console.log('Success! The SID for this SMS message is:');
-            console.log(message.sid);
-
-            console.log('Message sent on:');
-            console.log(message.dateCreated);
-        } else {
-            console.log('Oops! There was an error.');
-        }
-    });
+//    var client = new twilio.RestClient('', '');
+//    console.log(req.query['message']);
+//    
+//    client.sms.messages.create({
+//        to:'+<number>',
+//        from:'+<number>',
+//        body:req.query['message']
+//    }, function(error, message) {
+//        // The HTTP request to Twilio will run asynchronously. This callback
+//        // function will be called when a response is received from Twilio
+//        // The "error" variable will contain error information, if any.
+//        // If the request was successful, this value will be "falsy"
+//        if (!error) {
+//            // The second argument to the callback will contain the information
+//            // sent back by Twilio for the request. In this case, it is the
+//            // information about the text messsage you just sent:
+//            console.log('Success! The SID for this SMS message is:');
+//            console.log(message.sid);
+//
+//            console.log('Message sent on:');
+//            console.log(message.dateCreated);
+//        } else {
+//            console.log('Oops! There was an error.');
+//        }
+//    });
 });
 
 
